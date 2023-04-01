c597483d5d4d4354e2bceff65678adb0336b3a77
nandaUnical <rtgmfr98t59z504s@studenti.unical.it>
2023-04-02 00:50:59 +0200

    Frecuencia de las palabras

--- app_code/code.py
+++ app_code/code.py
@@ -1,10 +1,27 @@
 import sys
 import fileinput
 import re
+import os
+import csv
 
-def read_dir (dir_path):
-    pass
+#calcular la frecuencia de las palabras dado un directorio
+def read_dir_frec (dir_path):
+    frecuency = {}#dicc para las frecuencias
+    for fname in os.listdir(dir_path):
+        f = os.path.join(dir_path,fname)#path del fichero
+        if os.path.isfile(f) and fname.endswith('.txt'):#solo los .txt
+            file = open(f,'r')#abrir el fichero
+            for line in file :#iterando en cada linea
+                l = re.sub(r'[.,"\'-?:!;]', '', line)#eliminando signos de puntuacion
+                words = l.lower().split()
+                for w in words:#contando cada palabra
+                    if w in frecuency:
+                        frecuency[w] += 1
+                    else:
+                        frecuency[w] = 1
+    return frecuency
 
+#calcular la frecuencia de las palabras dado una lista de ficheros
 def read_file_list_frec ():
     
     frecuency = {} #dict para guardar las frecuencias
@@ -22,7 +39,19 @@ def read_file_list_frec ():
 
 if __name__ == "__main__":
     if len(sys.argv) == 2 :
-        read_dir(sys.argv[1])
+        frec = read_dir_frec(sys.argv[1])
     else:
-        read_file_list_frec()
+        frec = read_file_list_frec()
+
+    #guardar las frecuencias en un .csv
+    with open("..\\ejemplos\\frecuency.csv","w",newline="") as fp:
+        #create the writer object
+        writer = csv.DictWriter(fp, fieldnames=frec.keys())
+        #write the header row
+        writer.writeheader()
+        #write the data rows
+        writer.writerow(frec)
+
+
+    
 
