d250a15d7f44142e0a43d200576532fb1f7031e0
nandaUnical <rtgmfr98t59z504s@studenti.unical.it>
2023-04-01 18:41:22 +0200

    Importando code

--- app_code/code.py
+++ app_code/code.py
@@ -1,8 +1,28 @@
 import sys
+import fileinput
+import re
 
-def sum(a,b):
-    return a+b
+def read_dir (dir_path):
+    pass
 
+def read_file_list_frec ():
+    
+    frecuency = {} #dict para guardar las frecuencias
+    
+    for line in fileinput.input(encoding="utf-8"):#iterando sobre los sys.argv
+        l = re.sub(r'[.,"\'-?:!;]', '', line)#eliminando signos de puntuacion
+        words = l.lower().split()#lower case y separando por palabras
+        for w in words:
+            if w in frecuency:
+                frecuency[w] += 1
+            else:
+                frecuency[w] = 1
+
+    return frecuency
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) == 2 :
+        read_dir(sys.argv[1])
+    else:
+        read_file_list_frec()
+
--- tests/test_code.py
+++ tests/test_code.py
@@ -2,4 +2,4 @@ import pytest
 import app_code.code as cd
 
 def test_nothing ():
-    assert cd.sum(3,5) == 8
+    pass
