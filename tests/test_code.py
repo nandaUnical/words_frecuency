cfda8fe70c9ff883844262dc35048cd4de933bdc
nandaUnical <rtgmfr98t59z504s@studenti.unical.it>
2023-04-04 11:15:10 +0200

    Primer test

--- tests/test_code.py
+++ tests/test_code.py
@@ -7,7 +7,7 @@ import app_code.code as cd
 #2{'bien': 3, 'hola': 2, 'mundo': 2, 'como': 2, 'estan': 2, 'muy': 2, 'hoy': 2, 'todo': 2, 'por': 2, 'todos': 1, 'me': 1, 'siento': 1, 'deberiamos': 1, 'celebrarlo': 1, 'hasta': 1, 'pronto': 1, 'aqui': 1, 'gracias': 1, 'preguntar': 1}
 
 def test_read_dir_frec ():
-    dir_path = 'C:\\Users\\hp\\Documents\\!Master\\Agile\\!!Second Apello\\!proyecto\\words_frecuency\\ejemplos'
+    dir_path = '../ejemplos/'
     assert cd.read_dir_frec(dir_path) == {'bien': 5, 'todo': 5, 'por': 5, 'hola': 4, 'estan': 4, 'gracias': 4, 'mundo': 3, 'como': 3, 'aqui': 3, 'preguntar': 3, 'todos': 2, 'muy': 2, 'hoy': 2, 'gente': 2, 'me': 1, 'siento': 1, 'deberiamos': 1, 'celebrarlo': 1, 'hasta': 1, 'pronto': 1, 'la': 1}
 
 #def test_read_file_list_frec ():
