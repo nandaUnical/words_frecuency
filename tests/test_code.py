4eee6b7670c069e1611254c984e6669ab1182897
nandaUnical <rtgmfr98t59z504s@studenti.unical.it>
2023-04-07 23:42:10 +0200

    test dir path

--- tests/test_code.py
+++ tests/test_code.py
@@ -17,10 +17,11 @@ def test_read_dir_frec ():
     dir_path = os.path.join(BASE_DIR, "ejemplos")
     assert cd.read_dir_frec(dir_path) == {'bien': 5, 'todo': 5, 'por': 5, 'hola': 4, 'estan': 4, 'gracias': 4, 'mundo': 3, 'como': 3, 'aqui': 3, 'preguntar': 3, 'todos': 2, 'muy': 2, 'hoy': 2, 'gente': 2, 'me': 1, 'siento': 1, 'deberiamos': 1, 'celebrarlo': 1, 'hasta': 1, 'pronto': 1, 'la': 1}
 
-#def test_read_file_list_frec ():
-#    testargs = ["code.py", "../ejemplos/texto_1.txt", "../ejemplos/texto_2.txt", "../ejemplos/texto_3.txt", "../ejemplos/texto_4.txt", "../ejemplos/texto_5.txt"]
-#    with patch.object(sys, 'argv', testargs):
-#        assert cd.read_file_list_frec() == {'bien': 5, 'todo': 5, 'por': 5, 'hola': 4, 'estan': 4, 'gracias': 4, 'mundo': 3, 'como': 3, 'aqui': 3, 'preguntar': 3, 'todos': 2, 'muy': 2, 'hoy': 2, 'gente': 2, 'me': 1, 'siento': 1, 'deberiamos': 1, 'celebrarlo': 1, 'hasta': 1, 'pronto': 1, 'la': 1}
+def test_read_file_list_frec ():
+    dir_path = os.path.join(BASE_DIR, "ejemplos")
+    testargs = ["code.py", dir_path + "/texto_1.txt", dir_path + "/texto_2.txt", dir_path + "/texto_3.txt", dir_path + "/texto_4.txt", dir_path + "/texto_5.txt"]
+    with patch.object(sys, 'argv', testargs):
+        assert cd.read_file_list_frec() == {'bien': 5, 'todo': 5, 'por': 5, 'hola': 4, 'estan': 4, 'gracias': 4, 'mundo': 3, 'como': 3, 'aqui': 3, 'preguntar': 3, 'todos': 2, 'muy': 2, 'hoy': 2, 'gente': 2, 'me': 1, 'siento': 1, 'deberiamos': 1, 'celebrarlo': 1, 'hasta': 1, 'pronto': 1, 'la': 1}
 
 def test_most_frequent (mocker):
     k = 5
